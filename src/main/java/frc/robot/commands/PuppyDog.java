cf17d4c643f0320c815e7a74f21864417a278cfd
Ethan <ethan.ss.nyder@gmail.com>
2019-02-10 14:35:00 -0500

    PuppyDog in progress

--- src/main/java/frc/robot/commands/PuppyDog.java
+++ src/main/java/frc/robot/commands/PuppyDog.java
@@ -8,11 +8,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class PuppyDog extends Command {
-  public PuppyDog() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+
+  double area, x, y, tv, targetArea;
+  double speedRight, speedLeft;
+
+  public PuppyDog(double speedRight, double speedLeft, double targetArea) {
+    this.speedRight = speedRight;
+    this.speedLeft = speedLeft;
+    this.targetArea = targetArea;
   }
 
   // Called just before this Command runs the first time
@@ -23,6 +33,16 @@ public class PuppyDog extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double area =  NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+    double x = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+
+    while (tv == 0) {
+      Robot.drivetrain.set(0.3, -0.3);
+    }
+    while (tv == 1 && area <= 20.00d) {
+      Robot.drivetrain.set(speedLeft, speedRight);;
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
