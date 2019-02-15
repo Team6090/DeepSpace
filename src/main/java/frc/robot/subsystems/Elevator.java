ffb74fad68528cc66567cb7843261edc581b4d23
Jordan Bancino <jjbancino@gmail.com>
2019-02-14 20:48:49 -0500

    The Elevator will use a TalonSRX

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,12 +7,10 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANError;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-import com.revrobotics.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
@@ -27,39 +25,93 @@ import frc.robot.commands.ElevatorController;
  */
 public class Elevator extends Subsystem {
 
-  /* Set up the brushless elevator motor. */
-  private final CANSparkMax elevatorMotor = new CANSparkMax(RobotMap.elevatorMotor, MotorType.kBrushless);
+  /* Set up the elevator motor. */
+  private final WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(RobotMap.elevatorMotor);
   
+  private final int timeoutMs = 10;
 
   /**
    * Set up the elevator motor and subsystem.
    */
   public Elevator() {
-    /* TODO: PID Configuration goes here. */
-    double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
-    int slot = 1;
-    kP = 0.1;
-    kI = 0.001;
-    kD = 0;
-    kIz = 0;
-    kFF = 0;
-    kMaxOutput = 1;
-    kMinOutput = -1;
-    getPIDController().setP(kP, slot);
-    getPIDController().setI(kI, slot);
-    getPIDController().setD(kD, slot);
-    getPIDController().setFF(kFF, slot);
-    getPIDController().setIZone(kIz,slot);
-    getPIDController().setOutputRange(kMinOutput, kMaxOutput);
+    /*
+		 * Setup the closed loop parameters for the winch Talon. For the
+		 * SelectProfileSlot: First parameter is setpoint slot (0-4) Second parameter is
+		 * PID loop number - usually 0 for primary loop
+		 * 
+		 * kFeed forward=(Percent motor load to achieve max speed) * 1023/max spd in
+		 * encoder cnts/100ms To set-run full speed in manual joystick mode and read
+		 * talon speed feedback and output percent from the Self-Test web interface
+		 * roborio-6090-frc.local
+     * 
+     * Slot 0 is wench coarse movement UP and DOWN during Teleop (Slot 0, PID loop number=0)
+		 */
+    elevatorMotor.selectProfileSlot(0, 0);
+  
+    /* Feed Forward gain - PID loop num, value, timout */
+    elevatorMotor.config_kF(0, .40, timeoutMs);
+    /* Proportional gain - PID loop num, value, timout */
+    elevatorMotor.config_kP(0, 0.3, timeoutMs);
+    /* Integral gain - PID loop num, value, timout */
+    elevatorMotor.config_kI(0, 0.0001, timeoutMs);
+    /* Differential gain - PID loop num, value, timout */
+    elevatorMotor.config_kD(0, 0, timeoutMs);
+    /* This doesn't appear to do anything useful */
+    elevatorMotor.configAllowableClosedloopError(0, 50, timeoutMs);
+    /* Enable integral component when error gets below this threshold */
+    elevatorMotor.config_IntegralZone(0, 5, timeoutMs);
+    /* Number of msec to allow peak current */
+    elevatorMotor.configPeakCurrentDuration(50, timeoutMs);
+    /* Continuous allow current in Amps */
+    elevatorMotor.configContinuousCurrentLimit(80, timeoutMs);
+    /* Peak current in Amps */
+    elevatorMotor.configPeakCurrentLimit(68, timeoutMs);
+    /* If error is below threshold, consider to be "in position" (0.25=25%) */
+    elevatorMotor.configNeutralDeadband(0.03, timeoutMs);
     
+    /*
+		 * Set accel and cruise velocity to use to make trapezoid profile Cruise speed =
+		 * 95% of max speed = 3800*0.95 = 3610 Acc rate = Cruise spd in 0.5 sec =
+		 * 3610/0.5 = 7220
+     * 
+     * Winch motor has a magnetic pickup encoder hooked to talon
+		 */
+    elevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    /* Make sure talons blink green when position increasing */
+    elevatorMotor.setSensorPhase(true);
+    /* Invert the speed reference */
+		elevatorMotor.setInverted(true);
+
+		/* Set relevant frame periods to be at least as fast as periodic rate */
+		elevatorMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, timeoutMs);
+    elevatorMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, timeoutMs);
+    /* Cruize velociy in encoder/100msec */
+    elevatorMotor.configMotionCruiseVelocity(3800, timeoutMs);
+    /*  Accel */
+    elevatorMotor.configMotionAcceleration(5500, timeoutMs);
+    /* Zero the position encoder */
+    elevatorMotor.setSelectedSensorPosition(0, 0, timeoutMs);
+    
+    /*
+		 * Set Open and Close Loop ramp rate -- Higher values give slower ramp--
+		 * 
+		 * Rate is change in output / 10msec chunks Max Talon speed reference = 1023
+		 * counts internally so speed change = end speed - start speed
+     * 
+     * Rate = [1023-0] / [1000 msec] * 10 msec = 10
+		 */
+    elevatorMotor.configOpenloopRamp(0.5, timeoutMs);
+    /* Rate = [1023-0] / [500 msec] * 10 msec = 20 */
+		elevatorMotor.configClosedloopRamp(0.5, timeoutMs);
   }
 
   /**
    * Get the encoder position.
    * @return The encoder position of the elevator motor.
    */
-  public double getPosition() {
-    return elevatorMotor.getEncoder().getPosition();
+  public int getPosition() {
+    /* Get Winch drive motor Position from Talon SRX */
+    return elevatorMotor.getSelectedSensorPosition(0);
   }
 
   /**
@@ -80,50 +132,15 @@ public class Elevator extends Subsystem {
     elevatorMotor.set(speed);
   }
 
+  public void set(ControlMode mode, double reference) {
+    elevatorMotor.set(mode, reference);
+  }
+
   /**
    * Stop the elevator motor.
    */
   public void stop() {
     setSpeed(0.0);
-  }
-
-  /**
-   * Get the PID controller for the elevator motor.
-   * @return An instance of CANPIDController.
-   */
-  public CANPIDController getPIDController() {
-    return elevatorMotor.getPIDController();
-  }
-
-  /**
-   * Set the controller reference value based on the selected
-   * control mode.
-   * @param gain The value to set depending on the control mode.
-   * For basic duty cycle control this should be a value between
-   * -1 and 1 Otherwise: Voltage Control: Voltage (volts) Velocity
-   * Control: Velocity (RPM) Position Control: Position
-   * (Rotations) Current Control: Current (Amps)
-   * @param ctrl The control type.
-   * @return CANError Set to REV_OK if successful.
-   */
-  public CANError setReference(double gain, ControlType ctrl) {
-    return getPIDController().setReference(gain, ctrl);
-  }
-
-  /**
-   * Set the controller reference value based on the selected
-   * control mode.
-   * @param value The value to set depending on the control mode.
-   * For basic duty cycle control this should be a value between -1 and 1
-   * Otherwise: Voltage Control: Voltage (volts) Velocity Control:
-   * Velocity (RPM) Position Control: Position (Rotations)
-   * Current Control: Current (Amps)
-   * @param ctrl The control type to override with.
-   * @param pidSlot The PID slot to run this command in.
-   * @return CANError Set to REV_OK if successful
-   */
-  public CANError setReference(double value, ControlType ctrl, int pidSlot) {
-    return getPIDController().setReference(value, ctrl, pidSlot);
   }
 
   /**
