5083b303548da1019c2a315074423a9548b2235c
Jordan Bancino <jjbancino@gmail.com>
2019-02-14 20:50:25 -0500

    Removed unused import

--- src/main/java/frc/robot/commands/GyroSmoothTurn.java
+++ src/main/java/frc/robot/commands/GyroSmoothTurn.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.subsystems.DriveTrain;
 
 /**
  * Turn the robot while also moving it forwards or backwards.
