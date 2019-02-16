70f4fc1c189710c1aff2db75763ca9e2dddbcc01
Jordan Bancino <jjbancino@gmail.com>
2019-02-16 18:59:00 -0500

    Move vision commands to the vision subpackage

--- src/main/java/frc/robot/commands/vision/GaffTapeAlign.java
+++ src/main/java/frc/robot/commands/vision/GaffTapeAlign.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.commands.vision;
 
 import frc.robot.LimelightCommand;
 import frc.robot.Robot;
