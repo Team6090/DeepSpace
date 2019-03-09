675c8320f8ccfd8c510c2da3e92ede0d2a714e2d
Collin Heavner <chizzle8777@gmail.com>
2019-03-09 15:06:31 -0500

    Tuned elevator setpoints

--- src/main/java/frc/robot/commands/joystick/ElevatorController.java
+++ src/main/java/frc/robot/commands/joystick/ElevatorController.java
@@ -35,7 +35,7 @@ public class ElevatorController extends Command {
   /*
    * Position references for elevator setpoints
    */
-  private final int bottomBallRef = 7500, middleBallRef = 16390, topBallRef = 25070;
+  private final int bottomBallRef = 7500, middleBallRef = 16200, topBallRef = 25200;
   private final int bottomHatchRef = 0, middleHatchRef = 8300, topHatchRef = 17500;
 
   /*
