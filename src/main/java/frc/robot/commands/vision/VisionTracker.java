4a2a578f345f9a183c0e7086f75de9c87cb5e204
Collin Heavner <chizzle8777@gmail.com>
2019-03-09 13:29:12 -0500

    Made VisionTracker as a command

---
+++ src/main/java/frc/robot/commands/vision/VisionTracker.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.vision;
+
+import edu.wpi.first.wpilibj.command.Command ;
+import frc.robot.Robot;
+
+public class VisionTracker extends Command {
+
+  private final Command hatchAlignment = new HatchAlignment(5000l, 0.5d, 8.00d);
+  private final Command gaffTapeAlign = new GaffTapeAlign(0.5d, 1.3d, -1.0d, 1.0d, 2.0d, 5000l);
+  private Command currentCommand;
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    if (Robot.intake.armIsUp()) {
+      currentCommand = hatchAlignment;
+    } else {
+      currentCommand = gaffTapeAlign;
+    }
+    currentCommand.start();
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    currentCommand.cancel();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
